b3c71f8e59e3fba61ec5312d3ff9a85900bc052f
Shweta Shrinivas <shshri31@in.ibm.com>
2021-05-30 12:34:28 +0530

    First commit for project setup

---
+++ deliveryPlan/src/test/java/com/shwetashrinivas/e2open/deliveryPlan/DeliveryPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shwetashrinivas.e2open.deliveryPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
