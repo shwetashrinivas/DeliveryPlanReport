13df02f7a87558efe0604d312155c0708d39dfcb
Shweta Shrinivas <shshri31@in.ibm.com>
2021-05-30 14:03:05 +0530

    Service / Business Logic layer

---
+++ deliveryPlan/src/main/java/com/shwetashrinivas/e2open/service/DPServiceImpl.java
@@ -0,0 +1,45 @@
+package com.shwetashrinivas.e2open.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shwetashrinivas.e2open.entity.DeliveryPlan;
+import com.shwetashrinivas.e2open.repository.DeliveryPlanRepository;
+
+@Service
+public class DPServiceImpl{
+
+    @Autowired
+    DeliveryPlanRepository deliveryPlanReportRepository;    
+    
+    List<DeliveryPlan> findByPart(String part){
+    	return deliveryPlanReportRepository.findByPart(part);
+    }
+
+	List<DeliveryPlan> findByDepot(String depot){
+		return deliveryPlanReportRepository.findByDepot(depot);
+	}
+
+	List<DeliveryPlan> findByCustomer(String customer){
+		return deliveryPlanReportRepository.findByCustomer(customer);
+	}
+
+	List<DeliveryPlan> findByDateRange(LocalDate startDate, LocalDate endDate){
+		return deliveryPlanReportRepository.findByDateRange(startDate, endDate);
+	}
+
+	List<DeliveryPlan> findByDueDateBefore(LocalDate dueDate){
+		return deliveryPlanReportRepository.findByDueDateBefore(dueDate);
+	}
+
+	List<DeliveryPlan> findByOrderByCustomerAsc(String customer){
+		return deliveryPlanReportRepository.findByOrderByCustomerAsc(customer);	
+	}
+
+	List<DeliveryPlan> findByCustomerOrderByQuantityAsc(String customer){
+		return deliveryPlanReportRepository.findByCustomerOrderByQuantityAsc(customer);
+	}
+}
